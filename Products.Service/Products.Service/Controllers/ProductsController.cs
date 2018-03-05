0d450cb24985295e29b60a6692536d7d5f2af364
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-03-05 18:47:26 +0530

    Few changes

---
+++ Products.Service/Products.Service/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Products.Service.Models;
+using System.Collections.Generic;
+
+namespace Products.Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Products")]
+    public class ProductsController : Controller
+    {
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return null;
+        }
+
+
+    }
+}
