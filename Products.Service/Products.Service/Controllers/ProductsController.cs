35345e4dcfacbd6c3f533d8b1d61904c3d273411
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-03-05 18:53:10 +0530

    Added Products Get All

--- Products.Service/Products.Service/Controllers/ProductsController.cs
+++ Products.Service/Products.Service/Controllers/ProductsController.cs
@@ -8,10 +8,16 @@ namespace Products.Service.Controllers
     [Route("api/Products")]
     public class ProductsController : Controller
     {
+        readonly IEnumerable<Product> _products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return null;
+            return _products;
         }
 
 
