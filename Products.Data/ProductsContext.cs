634c3ecefb7f4c0a2d6d2b9f895a0b93dea49880
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-03-10 23:02:58 +0530

    To be continued

---
+++ Products.Data/ProductsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Domain;
+
+namespace Products.Data
+{
+    class ProductsContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.
+                // .UseInMemoryDatabase("Products");
+        }
+    }
+}
