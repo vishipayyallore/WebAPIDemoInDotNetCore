fd68e907c0f825066e669b2778213429462cd74c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-03-13 23:28:27 +0530

    Making Async call

--- Products.Service/Products.Service/Controllers/ProductsController.cs
+++ Products.Service/Products.Service/Controllers/ProductsController.cs
@@ -3,14 +3,15 @@ using Products.Domain;
 using Products.Data;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Products.Service.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Products")]
+    [Route("api/[Controller]")]
     public class ProductsController : Controller
     {
-        private readonly ProductsContext _productsContext;
+        private readonly ProductsContext _productsContext = new ProductsContext();
         readonly IEnumerable<Product> _products = new[]
         {
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
@@ -20,19 +21,20 @@ namespace Products.Service.Controllers
 
         public ProductsController()
         {
-            _productsContext = new ProductsContext();
-            _productsContext.Products.Add(new Product {Id = 4, Name = "Hammer", Category = "Hardware", Price = 16.99M});
+            if (_productsContext.Products.Count() != 0) return;
+            _productsContext.Products.Add(new Product { Id = 4, Name = "Hammer", Category = "Hardware", Price = 16.99M });
             _productsContext.Products.AddRange(_products);
             _productsContext.SaveChanges();
         }
 
         [HttpGet]
-        public IEnumerable<Product> GetAllProducts()
+        public async Task<IEnumerable<Product>> GetAllProducts()
         {
-            return _productsContext.Products.ToList();
+            return await Task.FromResult<IEnumerable<Product>>(_productsContext.Products.ToList());
         }
 
         [HttpGet("{id}", Name = "GetProduct")]
+        [Route("GetProduct")]
         public IActionResult GetProduct(int id)
         {
             var product = _products.FirstOrDefault( (p) => p.Id == id );
