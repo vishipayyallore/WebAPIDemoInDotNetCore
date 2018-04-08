141a4413996b3138f91083483431b47dd8c47c46
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-04-08 23:10:31 +0530

    Creating new project

---
+++ Products.Service/Source/Products.Data/ToDoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Domain;
+
+namespace Products.Data
+{
+    public class ToDoContext : DbContext
+    {
+
+        public DbSet<TodoItem> ToDoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "ToDoItems");
+        }
+    }
+}
