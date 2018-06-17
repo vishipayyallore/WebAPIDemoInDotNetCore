51407f4bb2d85cd17fa2a34d9fd4568d4418e584
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-06-17 20:50:17 +0530

    Added the interface

---
+++ Products.Service/Source/Products.Core/ITodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Domain;
+using System;
+
+namespace Products.Core
+{
+
+    public interface ITodoContext : IDisposable
+    {
+        DbSet<TodoItem> ToDoItems { get; set; }
+
+        int SaveChanges();
+    }
+
+}
