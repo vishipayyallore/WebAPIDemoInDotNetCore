336f7c8dd884af4ab8b1d1db732507f3f55710f0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-06-17 20:55:58 +0530

    Added Entity Framework's Context

---
+++ Products.Service/Source/Products.Data/ProductsContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Core;
+using Products.Domain;
+
+namespace Products.Data
+{
+    public class ProductsContext : DbContext, IProductsContext
+    {
+
+        public ProductsContext() : base(new DbContextOptions<ProductsContext>())
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "Products");
+        }
+    }
+}
