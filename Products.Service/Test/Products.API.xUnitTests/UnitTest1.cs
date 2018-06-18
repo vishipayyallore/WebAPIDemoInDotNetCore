ad00be09bfd9bfb54e4a38dcb715fcddf422e611
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-06-18 21:01:02 +0530

    Added Two Test Projects

---
+++ Products.Service/Test/Products.API.xUnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Products.API.xUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
