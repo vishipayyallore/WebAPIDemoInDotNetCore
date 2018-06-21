3303aba68761388a7624f142715a3d5605aeaf88
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-06-21 20:02:44 +0530

    Adding Products Controller

---
+++ Products.Service/Source/Products.API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Products.Core;
+using Products.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Products.API.Controllers
+{
+
+    [Produces("application/json")]
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+
+        // Variables
+        private readonly IProductsContext _productsContext;
+
+        public ProductsController(IProductsContext productsContext)
+        {
+            _productsContext = productsContext;
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return null;
+        }
+
+    }
+
+}
